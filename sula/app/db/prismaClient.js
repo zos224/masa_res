29b56fc856aa27ee217e2b4694729ded8bdfccfa
zos224 <95456855+zos224@users.noreply.github.com>
2024-04-10 08:52:36 +0700

    Update prismaClient.js

--- sula/app/db/prismaClient.js
+++ sula/app/db/prismaClient.js
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
+let prisma;
+
 if (process.env.NODE_ENV === "production") {
-    const prisma = new PrismaClient();
-}
-else {
+    prisma = new PrismaClient();
+} else {
     if (!global.prisma) {
         global.prisma = new PrismaClient();
     }
-    const prisma = global.prisma;
+    prisma = global.prisma;
 }
 
 export default prisma;
