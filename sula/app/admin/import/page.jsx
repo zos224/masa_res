a63ba49c76ea31f5fc040ecaa8475f727f823e9b
zos224 <95456855+zos224@users.noreply.github.com>
2024-04-10 08:57:09 +0700

    Update page.jsx

--- sula/app/admin/import/page.jsx
+++ sula/app/admin/import/page.jsx
@@ -1,7 +1,7 @@
 import { promises as fs } from 'fs';
 
 export default async function Page() {
-    const file = await fs.readFile('/public/data.json', 'utf8');
+    const file = await fs.readFile('data.json', 'utf8');
     const data = JSON.parse(file);
     const formData = new FormData();
     formData.append('data', JSON.stringify(data));
